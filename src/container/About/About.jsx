c5165c272e0107a999877be7b5fd5b3bb3dba08f
sekharjavvadi <sekharatece@gmail.com>
2022-12-11 08:23:13 +0530

    implemented suspense but didn't worked

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect, Suspense } from "react";
+import { motion } from "framer-motion";
 
-import { AppWrap, MotionWrap } from '../../wrapper';
-import './About.scss';
-import { urlFor, client } from '../../client';
+import { AppWrap, MotionWrap } from "../../wrapper";
+import "./About.scss";
+import { urlFor, client } from "../../client";
 import aboutJson from "../../json_files/about.json";
 
 const About = () => {
@@ -15,26 +15,35 @@ const About = () => {
 
     // client.fetch(query).then((data) => {
     //   setAbouts(data);
-      
+
     // });
   }, []);
 
   return (
     <>
-      <h2 className="head-text">What We <span>Offer</span></h2>
+      <h2 className="head-text">
+        What We <span>Offer</span>
+      </h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
+            transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.image ?? urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+            <Suspense fallback={<h1>Loading...</h1>}>
+             
+              <img src={about?.image}  />
+            </Suspense>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about?.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about?.description}
+            </p>
           </motion.div>
         ))}
       </div>
@@ -42,8 +51,9 @@ const About = () => {
   );
 };
 
+
 export default AppWrap(
-  MotionWrap(About, 'app__about'),
-  'about',
-  'app__whitebg',
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
 );
